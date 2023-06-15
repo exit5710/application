2a06e2afd853b09749bc41291c9e05af2361905e
exit5710 <exit5710@gmail.com>
2023-06-15 20:21:47 +0900

    training

--- unit/src/training/format/FormatDriver.java
+++ unit/src/training/format/FormatDriver.java
@@ -6,5 +6,12 @@ public class FormatDriver {
 
 		String result = String.format("문자열 서식: %s, %S, %d", str, str, 11);
 		System.out.println(result);
+
+		String data = "firstName=111&lastName=22&gender=f&minutes=33&seconds=44&action=addRunner";
+		data = "{" + data + "}";
+		data = data.replaceAll("=", " : ");
+		data = data.replaceAll("&", ", ");
+
+		System.out.println(data);
 	}
 }
