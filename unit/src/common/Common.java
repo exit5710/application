d979708958f9939f1050b32f8879531777048692
exit5710 <exit5710@gmail.com>
2023-07-03 23:00:50 +0900

    common

---
+++ unit/src/common/Common.java
@@ -0,0 +1,70 @@
+package common;
+
+import java.util.Scanner;
+
+public class Common {
+	public static String setInputString() {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		scanner.close();
+
+		return input;
+	}
+
+	public static int setInputInteger() {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		scanner.close();
+
+		return input;
+	}
+
+	public static String[] setInputArray() {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		scanner.close();
+
+		return input.split(" ");
+	}
+
+	public static int getMaxValue(int[] valueList) {
+		int maxValue = 0;
+
+		for (int value : valueList) {
+			if (value > maxValue) {
+				maxValue = value;
+			}
+		}
+		return maxValue;
+	}
+
+	public static boolean isNumeric(String value) {
+		// value.matches("[+-]?\\d*(\\.\\d+)?");
+		boolean bool = true;
+
+		try {
+			Integer.parseInt(value);
+		} catch (NumberFormatException e) {
+			bool = false;
+		}
+
+		return bool;
+	}
+
+	public static int[] getArrayNumber(String[] listValue) {
+		int[] arrayNumber = new int[listValue.length];
+		int count = 0;
+
+		for (String value : listValue) {
+			if (isNumeric(value)) {
+				arrayNumber[count] = Integer.parseInt(value);
+				count++;
+			}
+		}
+
+		int[] newArray = new int[count];
+		System.arraycopy(arrayNumber, 0, newArray, 0, count);
+
+		return newArray;
+	}
+}
