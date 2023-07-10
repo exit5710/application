55f757f134e5fb9f97cb8dca0a62332dbe3320fb
exit5710 <exit5710@gmail.com>
2023-07-10 18:53:48 +0900

    springToby ResourceRead

--- springToby/src/main/java/ResourceRead.java
+++ springToby/src/main/java/ResourceRead.java
@@ -2,19 +2,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ResourceRead {
-	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	private static final Class<?> clazz = MethodHandles.lookup().lookupClass();
+	private static final Logger logger = LoggerFactory.getLogger(clazz);
 
 	public static void main(String[] args) {
-		logger.info(String.valueOf(MethodHandles.lookup().lookupClass()));
+		logger.info(String.valueOf(clazz));
 		// C:\java\IdeaProjects\webApp\out\artifacts\spring5fs_war_exploded\WEB-INF\classes\dataBaseInfo.properties
-		// Class<?> c = MethodHandles.lookup().lookupClass();
 		// String path = Test.class.getResource("dataBaseInfo.properties").getPath();
-		String path = MethodHandles.lookup().lookupClass().getResource("dataBaseInfo.properties").getPath();
+		String path = clazz.getResource("dataBaseInfo.properties").getPath();
 
 		try {
 			FileReader resources = new FileReader(path);
@@ -25,18 +26,16 @@ public class ResourceRead {
 			String user = properties.getProperty("mariaDbUser");
 			String password = properties.getProperty("mariaDbPassword");
 
-			System.out.println("path : " + path);
-			System.out.println("url : " + url);
-			System.out.println("user : " + user);
-			System.out.println("password : " + password);
+			logger.info("path : " + path);
+			logger.info("url : " + url);
+			logger.info("user : " + user);
+			logger.info("password : " + password);
 
 			throw new SQLException();
-		} catch (ArithmeticException e) {
-			System.err.println("ArithmeticException");
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println("ArrayIndexOutOfBoundsException");
-		} catch (Exception e) {
+		} catch (IOException | SQLException e) {
 			logger.debug(e.toString());
+		} catch (Exception e) {
+			logger.error(e.toString());
 		}
 	}
 }
