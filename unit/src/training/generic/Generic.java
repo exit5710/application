7bcf0219d1357c957e8bc46dddbb5b17e3b4ff71
exit5710 <exit5710@gmail.com>
2023-09-23 18:36:37 +0900

    Generic

---
+++ unit/src/training/generic/Generic.java
@@ -0,0 +1,24 @@
+package training.generic;
+
+class Man<T> {
+    private T temp;
+
+    public T getTemp() {
+        return temp;
+    }
+    public void setTemp(T temp) {
+        this.temp = temp;
+    }
+}
+
+public class Generic {
+    public static void main(String[] args) {
+        Man<String> manString = new Man<>();
+        manString.setTemp("String");
+        System.out.println(manString.getTemp());
+
+        Man<Integer> manInteger = new Man<>();
+        manInteger.setTemp(20);
+        System.out.println(manInteger.getTemp());
+    }
+}
