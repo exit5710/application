273f96d3cb6ce61a50983e6271d3a4763ac4918d
exit5710 <exit5710@gmail.com>
2024-02-22 00:42:56 +0900

    SpringApp patternizer

--- springApp/src/main/java/unitApp/patternizer/PatternizerApp.java
+++ springApp/src/main/java/unitApp/patternizer/PatternizerApp.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PatternizerApp {
 	private static final Class<?> thisClass = MethodHandles.lookup().lookupClass();
 	private static final Logger logger = LoggerFactory.getLogger(thisClass);
@@ -12,23 +15,19 @@ public class PatternizerApp {
 	public static void main(String[] args) {
 		logger.info(String.valueOf(thisClass));
 
-		String dataFrom1 = "A.NAME = #{name}";
-		String dataFrom2 = "<![CDATA[ A.AGE <= 19 ]]>";
-		String dataFrom3 = "<if test='name != null and name != \"\"'>";
-		String dataFrom4 = "/* this is comment */";
-		String dataFrom5 = "/* this is comment \n" + "test 111 */";
+		List<String> list = new ArrayList<>();
+		list.add("A.NAME = #{name}");
+		list.add("<![CDATA[ A.AGE <= 19 ]]>");
+		list.add("<if test='name != null and name != \"\"'>");
+		list.add("</if>");
+		list.add("<where>");
+		list.add("</where>");
+		list.add("/* this is comment */");
+		list.add("/* this is comment \n" + "test 111 */");
 
-		String regexData1 = processRegex(dataFrom1);
-		String regexData2 = processRegex(dataFrom2);
-		String regexData3 = processRegex(dataFrom3);
-		String regexData4 = processRegex(dataFrom4);
-		String regexData5 = processRegex(dataFrom5);
-
-		logger.debug(regexData1);
-		logger.debug(regexData2);
-		logger.debug(regexData3);
-		logger.debug(regexData4);
-		logger.debug(regexData5);
+		for (String element : list) {
+			logger.debug(processRegex(element));
+		}
 	}
 
 	// 문자열 처리 메서드
@@ -41,6 +40,11 @@ public class PatternizerApp {
 
 		// if 태그 패턴 처리
 		data = data.replaceAll("<if[^>]*test='[^']*'[^>]*>", "");
+		data = data.replaceAll("</if>", "");
+
+		// where 태그 패턴 처리
+		data = data.replaceAll("<where>", "WHERE 1 = 1");
+		data = data.replaceAll("</where>", "");
 
 		// 주석 패턴 처리
 		data = data.replaceAll("//.*", "");
