36084feadfe6b27ebbaa000bab53260a765ec62e
exit5710 <exit5710@gmail.com>
2024-03-09 16:07:12 +0900

    SpringApp ConnectionLocator

--- springApp/src/main/java/config/database/ConnectionLocator.java
+++ springApp/src/main/java/config/database/ConnectionLocator.java
@@ -1,10 +1,8 @@
 package config.database;
 
-import java.io.IOException;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface ConnectionLocator {
-	Connection getConnection() throws SQLException, IOException;
+	Connection getConnection() throws SQLException;
 }
