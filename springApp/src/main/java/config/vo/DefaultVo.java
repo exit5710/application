3f06c0920f716795897226d037a6798dd13fed13
exit5710 <exit5710@gmail.com>
2024-03-13 19:50:57 +0900

    SpringApp DefaultVo

---
+++ springApp/src/main/java/config/vo/DefaultVo.java
@@ -0,0 +1,78 @@
+package config.vo;
+
+import java.io.Serializable;
+
+public class DefaultVo implements Serializable {
+	private String rowStatus;
+	private String gridId;
+	private int resultCnt;
+	private String message;
+	private String registerId;
+	private String registerDate;
+	private String modifyId;
+	private String modifyDate;
+
+	public String getRowStatus() {
+		return rowStatus;
+	}
+
+	public void setRowStatus(String rowStatus) {
+		this.rowStatus = rowStatus;
+	}
+
+	public String getGridId() {
+		return gridId;
+	}
+
+	public void setGridId(String gridId) {
+		this.gridId = gridId;
+	}
+
+	public int getResultCnt() {
+		return resultCnt;
+	}
+
+	public void setResultCnt(int resultCnt) {
+		this.resultCnt = resultCnt;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRegisterId() {
+		return registerId;
+	}
+
+	public void setRegisterId(String registerId) {
+		this.registerId = registerId;
+	}
+
+	public String getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(String registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	public String getModifyId() {
+		return modifyId;
+	}
+
+	public void setModifyId(String modifyId) {
+		this.modifyId = modifyId;
+	}
+
+	public String getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+}
