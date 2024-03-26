2bd93a621b30e85421f5a42eae72e4076471ec1e
exit5710 <exit5710@gmail.com>
2024-03-26 21:15:10 +0900

    SpringApp ReflectionConnector

--- springApp/src/main/java/config/database/reflection/ReflectionConnector.java
+++ springApp/src/main/java/config/database/reflection/ReflectionConnector.java
@@ -1,13 +1,14 @@
-package config.database;
+package config.database.reflection;
 
+import config.database.DatabaseLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class ReflectConnector {
-	private static final Logger logger = LoggerFactory.getLogger(ReflectConnector.class);
+public class ReflectionConnector {
+	private static final Logger logger = LoggerFactory.getLogger(ReflectionConnector.class);
 
 	public static Connection getConnection(String databaseName) throws SQLException {
 		logger.debug("ReflectConnector getConnection: {}", databaseName);
